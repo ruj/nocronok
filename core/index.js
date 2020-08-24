05a45f9af6d3f42cd9b5a15c34323eacc14160b0
tenasatupitsyn <214782@bk.ru>
2020-08-24 04:39:07 -0300

    Base folder name in lower case

--- core/index.js
+++ core/index.js
@@ -2,7 +2,7 @@ const CLIENT_OPTIONS = {
   disableMentions: 'everyone'
 }
 
-const { Nocronok } = require('./structures/Base')
+const { Nocronok } = require('./structures/base')
 const client = new Nocronok(CLIENT_OPTIONS)
 
 client.log('Starting')
