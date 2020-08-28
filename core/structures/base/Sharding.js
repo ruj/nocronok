13217c472b156578820f26f895126a7babbf71c4
tenasatupitsyn <214782@bk.ru>
2020-08-28 04:13:51 -0300

    Event of shard creation passed to a function

--- core/structures/base/Sharding.js
+++ core/structures/base/Sharding.js
@@ -4,6 +4,10 @@ module.exports = class Sharding extends ShardingManager {
   constructor () {
     super('core/index.js')
 
-    this.on('shardCreate', (shard) => console.log(`Launching shard ${shard.id + 1}/${this.totalShards}`))
+    this.on('shardCreate', (...v) => this.onShardCreate(...v))
+  }
+
+  onShardCreate (shard) {
+    console.log(`Launching shard (${shard.id + 1}/${this.totalShards})`)
   }
 }
