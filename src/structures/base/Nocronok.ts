00849ec98fe693811ed63ec813adba781708f4d1
ruj <214782@bk.ru>
2023-07-30 23:01:22 -0300

    feat: logger

--- src/structures/base/Nocronok.ts
+++ src/structures/base/Nocronok.ts
@@ -1,9 +1,15 @@
 import { Client, GatewayIntentBits } from 'discord.js'
 
+import Logger from './Logger'
+
 export default class Nocronok extends Client {
+  public logger
+
   constructor () {
     super({
       intents: [GatewayIntentBits.Guilds]
     })
+
+    this.logger = new Logger({ prettyPrint: true })
   }
 }
