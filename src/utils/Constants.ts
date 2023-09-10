6b7c50af705572f4f42e6fee903bc098091c8446
ruj <214782@bk.ru>
2023-09-10 16:57:59 -0300

    chore(option): environment variable sanitizer

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,8 +1,12 @@
+export const EnvVars = ['PORT', 'HTTP_PASSWORD']
+
 export const Errors = {
   Loaders: {
     HttpLoader: {
-      PORT_IS_NOT_SET: 'Server not started - Environment variable "PORT" is not set',
-      HTTP_PASSWORD_IS_NOT_SET: 'Unprotected requests - Environment variable "HTTP_PASSWORD" is not set'
+      PORT_IS_NOT_SET:
+        'Server not started - Environment variable "PORT" is not set',
+      HTTP_PASSWORD_IS_NOT_SET:
+        'Unprotected requests - Environment variable "HTTP_PASSWORD" is not set'
     }
   }
 }
--- src/utils/Options.ts
+++ src/utils/Options.ts
@@ -2,10 +2,24 @@ import pick from 'lodash/pick'
 
 import { IDefaultOptions } from '@interfaces'
 
+import { EnvVars } from './Constants'
+
 export default class Options extends null {
   public static defaultOptions (): IDefaultOptions {
     return {
-      env: pick(process.env, ['PORT', 'HTTP_PASSWORD'])
+      env: Options.sanitizedEnvs()
     }
+  }
+
+  private static sanitizedEnvs (envVars: string[] = EnvVars) {
+    const envs = {} as IDefaultOptions['env']
+
+    for (const [key, value] of Object.entries(pick(process.env, envVars))) {
+      if (key) {
+        envs[key as keyof typeof envs] = value && JSON.parse(value)
+      }
+    }
+
+    return envs
   }
 }
