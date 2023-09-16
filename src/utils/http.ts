e7ddb55734d44153ddb8a29001d822840e74251f
ruj <214782@bk.ru>
2023-09-16 19:50:34 -0300

    feat: http utility functions

---
+++ src/utils/http.ts
@@ -0,0 +1,19 @@
+import fetch from 'cross-fetch'
+import merge from 'lodash/merge'
+
+type Payload = {
+  [key: string]: any
+}
+
+type Headers = Payload
+
+const defaultHeaders = {
+  'Content-Type': 'application/json'
+}
+
+const response = <T>(response: any): Promise<T> =>
+  response.headers.get('Content-Type').startsWith('application/json') &&
+  (response.json() as Promise<{ data: T }>)
+
+export const GET = (path: string, headers: Headers = defaultHeaders) =>
+  fetch(path, { headers: merge(defaultHeaders, headers) }).then(response)
