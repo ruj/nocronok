2d801bc7e014b43a4a655d4fd5a04df2def9e015
ruj <214782@bk.ru>
2023-09-16 19:46:27 -0300

    chore(enum): environment variable enumerator can be general

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,1 +1,5 @@
-export * from './option'
+export enum EEnvironmentVariables {
+  PORT = 'PORT',
+  HTTP_PASSWORD = 'HTTP_PASSWORD',
+  DEVELOPERS_ID = 'DEVELOPERS_ID'
+}
--- src/enums/option.ts
+++
@@ -1,5 +0,0 @@
-export enum EEnvironmentVariables {
-  PORT = 'PORT',
-  HTTP_PASSWORD = 'HTTP_PASSWORD',
-  DEVELOPERS_ID = 'DEVELOPERS_ID'
-}
