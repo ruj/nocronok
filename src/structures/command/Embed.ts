011a2b8e19d5caa4703821cde974a55b6e3f1890
ruj <214782@bk.ru>
2023-10-22 19:28:54 -0300

    feat(command): embed

---
+++ src/structures/command/Embed.ts
@@ -0,0 +1,7 @@
+import { EmbedBuilder } from '@discordjs/builders'
+
+export default class Embed extends EmbedBuilder {
+  constructor (data = {}) {
+    super(data)
+  }
+}
--- src/structures/command/index.ts
+++ src/structures/command/index.ts
@@ -2,5 +2,6 @@ import { SlashCommandBuilder } from '@discordjs/builders'
 
 import Command from './Command'
 import Context from './Context'
+import Embed from './Embed'
 
-export { Command, Context, SlashCommandBuilder }
+export { Command, Context, Embed, SlashCommandBuilder }
