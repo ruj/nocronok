311b1e60524213a709f58d12ca6d9b14a000347c
ruj <214782@bk.ru>
2023-11-18 22:28:25 -0300

    chore(api): option of environment variables required for api operation

--- src/loaders/ApiLoader.ts
+++ src/loaders/ApiLoader.ts
@@ -1,3 +1,4 @@
+import ApiWrapper from '@structures/ApiWrapper'
 import type Nocronok from '@structures/base/Nocronok'
 import Loader from '@structures/Loader'
 
@@ -13,6 +14,27 @@ export default class ApiLoader extends Loader {
   public async loadFile (Api: any) {
     const api = new Api()
 
+    this.addApi(api)
+  }
+
+  private async addApi (api: ApiWrapper) {
+    if (
+      !api.envVars?.every((variable) => {
+        if (!process.env[variable]) {
+          this.logger.warn(
+            { labels: ['ApiLoader', 'addApi()'] },
+            `${api.name} failed to load - Required environment variable "${variable}" is not set.`
+          )
+        }
+
+        return !!process.env[variable]
+      })
+    ) {
+      return false
+    }
+
     this.client.apis[api.name] = await api.load()
+
+    return true
   }
 }
