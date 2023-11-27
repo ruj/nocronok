fbf40a9bdb6b56bc3abbd1577e225f865695f2b0
ruj <214782@bk.ru>
2023-11-26 23:25:45 -0300

    chore: environment variables for webhook

--- src/@types/environment.d.ts
+++ src/@types/environment.d.ts
@@ -5,6 +5,9 @@ declare global {
       HTTP_PASSWORD?: string
       DEVELOPERS_ID?: string
       DISCORD_TOKEN: string
+      DISCORD_DEFAULT_WEBHOOK_ID: string
+      DISCORD_DEFAULT_WEBHOOK_TOKEN: string
+      STEAM_API_KEY: string
     }
   }
 }
